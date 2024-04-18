07e4848b5adfc8210ae7f2d8884a0ff909f2d4cb
Hristi <borisarroga@gmail.com>
2024-04-18 10:53:31 +0200

    I modified the views to show the old homepage

--- src/mbconsulting/views.py
+++ src/mbconsulting/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 
 def home_old(request):
-    return render(request, "home.html")
+    # return render(request, "home.html")
+    return HttpResponse("<h1>c'est bon<h1>")
